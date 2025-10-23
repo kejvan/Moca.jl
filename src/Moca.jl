af0c1c7b2b4209a2a10c95e8484c313894b7f9cc
Keivan <kejvan@protonmail.com>
2025-10-23 16:09:21 -0400

    Add ornstein-uhlenbeck process

--- src/Moca.jl
+++ src/Moca.jl
@@ -4,7 +4,7 @@ include("rng.jl")
 export RNGStrategy, StandardRNG, split_rng, rand_normal, rand_uniform
 
 include("processes.jl")
-export StochasticProcess, WienerProcess, GeometricBrownianMotion, step
+export StochasticProcess, WienerProcess, GeometricBrownianMotion, OrnsteinUhlenbeckProcess, step
 
 include("sampling.jl")
 export SamplingStrategy, StandardSampling, AntitheticSampling
--- src/processes.jl
+++ src/processes.jl
@@ -42,6 +42,33 @@ function step(
     dt::Float64,
     Z::Float64
 )::Float64
-    dS = process.drift * current_state * dt + process.volatility * current_state * sqrt(dt) * Z
+    dS =
+        process.drift * current_state * dt +
+        process.volatility * current_state * sqrt(dt) * Z
     return current_state + dS
+end
+
+"""
+Mean-reverting process: dX = mean_reversion_speed * (long_term_mean - X) * dt + volatility * dW
+"""
+@kwdef struct OrnsteinUhlenbeckProcess <: StochasticProcess
+    mean_reversion_speed::Float64
+    long_term_mean::Float64
+    volatility::Float64
+end
+
+"""
+    step(process::OrnsteinUhlenbeckProcess, current_state, dt, Z)
+
+Advance process by dt using Euler-Maruyama discretization. Z is a standard normal random variable.
+"""
+function step(
+    process::OrnsteinUhlenbeckProcess,
+    current_state::Float64,
+    dt::Float64,
+    Z::Float64
+)::Float64
+    dX =
+        process.mean_reversion_speed * (process.long_term_mean - current_state) * dt + process.volatility * sqrt(dt) * Z
+    return current_state + dX
 end
